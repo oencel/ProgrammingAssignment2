aa4d51eef3f01acad2a4cc7292df294073999a4e
oencel <oncel_ilhan@windowslive.com>
2015-09-25 18:30:42 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##Generate a function to create a special matrix taht can cache its inverse
+makeCacheMatrix <- function(x = matrix()) { 
+  ## initialize the matrix inverse value to NULL
+  matrixinverse <- NULL                     
+  set <- function(y) {                      
+    x <<- y
+    ## change the value of inverse of the matrix incase the matrix was changed.
+    matrixinverse <<- NULL              
+  }
+  ## takes the value of the inverse
+  get <- function() x                           
+  #calculates the inverse of non-singular matrix via the solve function
+  setinverse <- function(solve) matrixinverse <<- solve 
+  # takes the inverse     
+  getinverse <- function() matrixinverse        
+  ## passes the value of the function makeCacheMatrix        
+  list(set = set, get = get,                    
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##Generate a function to calculate the inverse. If inverse has already been computed
+##then the cacheSolve should retrieve the inverse from the cash
+cacheSolve<- function(x, ...) {                 
+  matrixinverse <- x$getinverse()
+  #if there is inverse, it gets it.
+  if(!is.null(matrixinverse)) {                 
+    message("getting cached data - Inverse of the matrix")
+    return(matrixinverse)
+  }
+  #if the inverse is not there, it is computed and then retrieved.
+  data <- x$get()                               
+  matrixinverse <- solve(data, ...)
+  x$setinverse(matrixinverse)
+  matrixinverse
 }
